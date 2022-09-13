8cdbf0f97f75296d32c99b6338403f9839d247b9
Mat Greten <mgreten@gmail.com>
2022-09-13 13:45:01 -0600

    feature: checking for incomplete required tasks & setting GitHub PR check status

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Require tasks to be completed in a PR
 
-![you shall not pass](https://knowyourmeme.com/memes/you-shall-not-pass)
+Checks for completion of required tasks.
+
+Required task syntax:
+
+```md
+- [ ] <!-- required task -->
+```
+ Regular tasks `- [ ]` are ignored.
+
+![you shall not pass](https://i.kym-cdn.com/entries/icons/original/000/002/144/You_Shall_Not_Pass!_0-1_screenshot.jpg)
--- index.js
+++ index.js
@@ -2,7 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const main = async () => {
+  const name = 'PR Required Tasks';
 
+  try {
+    const token = core.getInput('token', { required: true });
+    const body = core.getInput('body', { required: true });
+
+    const incompleteTasks = body.includes("- [ ] <!-- required task -->")
+
+    const githubApi = new github.GitHub(token)
+
+    await githubApi.checks.create({
+      name,
+      head_sha: github.context.payload.pull_request?.head.sha,
+      status: 'completed',
+      conclusion: incompleteTasks ? 'failure' : 'success',
+      completed_at: new Date().toISOString(),
+      output: {
+        title: name,
+        summary: incompleteTasks ? 'Some required tasks are incomplete ❌' : 'All required tasks complete ✅',
+      },
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    })
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main();
